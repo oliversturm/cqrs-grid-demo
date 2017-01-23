7f2ab49bce22d01289c03e230d02017717732a14
Oliver Sturm <oliver@oliversturm.com>
2017-01-23 14:06:36 +0000

    changed query tests to work without commands tests added for skip and take

--- query-service/tests/tests.js
+++ query-service/tests/tests.js
@@ -1,33 +1,43 @@
 const expect = require("chai").expect;
 const Seneca = require("seneca");
 
+const TESTRECORD_COUNT = 100;
+
 function testQueryValues(tdone, test) {
     const db = require("../../db")({
 	mongoHost: "localhost",
 	mongoDbName: "valuedb_test"
     });
 
+    
     db(db => {
 	db.dropDatabase((err, res) => {
-	    const seneca = Seneca({
-		log: "test"
+
+	    const values = db.collection("values");
+	    Promise.all(
+		Array.from(new Array(TESTRECORD_COUNT), (v, i) => i + 1).map(
+		    n => values.insertOne({
+			test: n % 10,
+			val: "Item " + n
+		    })
+		)
+	    ).then(() => {
+		const seneca = Seneca({
+		    log: "test"
+		});
+		
+		seneca.test(tdone/* use this for test debugging *//*, "print"*/).
+		    use(require("../query-values"), {
+			connectedDb: db
+		    }).ready(() => {
+			test(seneca, () => {
+			    seneca.close();
+			    db.close();
+			    tdone();
+			});
+		    });	
 	    });
-	    
-	    seneca.test(tdone/* use this for test debugging *//*, "print"*/).
-		use(require("../../validator/validator")).
-		use(require("../../command-service/command-values"), {
-		    connectedDb: db
-		}).
-		use(require("../query-values"), {
-		    connectedDb: db
-		}).ready(() => {
-		    test(seneca, () => {
-			seneca.close();
-			db.close();
-			tdone();
-		    });
-		});	    
-	});
+	});	
     });
 }
 
@@ -36,35 +46,16 @@ describe("query-values", function() {
 	it("list should retrieve all entities", function(tdone) {
 	    testQueryValues(tdone, (seneca, ldone) => {
 		seneca.act({
-		    role: "entitiesCommand",
+		    role: "entitiesQuery",
 		    domain: "values",
-		    cmd: "create",
-		    instance: {
-			test: 42,
-			val: "something"
-		    }
+		    cmd: "list"
 		}, function(err, res) {
-		    expect(err, "saveerr").to.be.null;
-		    const newObjectId = res.id;
+		    expect(err, "err").to.be.null;
+		    expect(res.totalCount).to.eql(TESTRECORD_COUNT);
 		    
-		    seneca.act({
-			role: "entitiesQuery",
-			domain: "values",
-			cmd: "list"
-		    }, function(err, res) {
-			expect(err, "err").to.be.null;
-			expect(res.totalCount).to.eql(1);
-			
-			expect(res.data, "res.data").to.be.instanceof(Array);
-			expect(res.data, "result").to.have.lengthOf(1);
-			expect(res.data[0], "result item").to.eql({
-			    _id: newObjectId,
-			    test: 42,
-			    val: "something"
-			});
-			
-			ldone();
-		    });
+		    expect(res.data, "res.data").to.be.instanceof(Array);
+		    expect(res.data, "result").to.have.lengthOf(TESTRECORD_COUNT);
+		    ldone();
 		});
 	    });
 	});
@@ -72,35 +63,68 @@ describe("query-values", function() {
     	it("fetch should retrieve one entity by id", function(tdone) {
 	    testQueryValues(tdone, (seneca, ldone) => {
 		seneca.act({
-		    role: "entitiesCommand",
+		    role: "entitiesQuery",
 		    domain: "values",
-		    cmd: "create",
-		    instance: {
-			test: 42,
-			val: "something"
-		    }
+		    cmd: "list"
 		}, function(err, res) {
-		    expect(err, "saveerr").to.be.null;
-		    const newObjectId = res.id;
+		    expect(err, "listerr").to.be.null;
+		    const testObject = res.data[0];
+		    console.log("test object", testObject);
 		    
 		    seneca.act({
 			role: "entitiesQuery",
 			domain: "values",
 			cmd: "fetch",
-			id: newObjectId
+			id: testObject._id
 		    }, function(err, res) {
 			expect(err, "err").to.be.null;
-			expect(res, "result").to.eql({
-			    _id: newObjectId,
-			    test: 42,
-			    val: "something"
-			});
+			expect(res, "result").to.eql(testObject);
 			
 			ldone();
 		    });
 
 		});
 		
+	    });
+	});
+
+	it("list should accept skip", function(tdone) {
+	    testQueryValues(tdone, (seneca, ldone) => {
+		seneca.act({
+		    role: "entitiesQuery",
+		    domain: "values",
+		    cmd: "list",
+		    params: {
+			skip: 5
+		    }
+		}, function(err, res) {
+		    expect(err, "err").to.be.null;
+		    expect(res.totalCount).to.eql(TESTRECORD_COUNT);
+		    
+		    expect(res.data, "res.data").to.be.instanceof(Array);
+		    expect(res.data, "result").to.have.lengthOf(TESTRECORD_COUNT - 5);
+		    ldone();
+		});
+	    });
+	});
+
+	it("list should accept take", function(tdone) {
+	    testQueryValues(tdone, (seneca, ldone) => {
+		seneca.act({
+		    role: "entitiesQuery",
+		    domain: "values",
+		    cmd: "list",
+		    params: {
+			take: 5
+		    }
+		}, function(err, res) {
+		    expect(err, "err").to.be.null;
+		    expect(res.totalCount).to.eql(TESTRECORD_COUNT);
+		    
+		    expect(res.data, "res.data").to.be.instanceof(Array);
+		    expect(res.data, "result").to.have.lengthOf(5);
+		    ldone();
+		});
 	    });
 	});
     });
