9e5a08cf1721249b67d6bf28f5c81b2af68eeebe
Oliver Sturm <oliver@oliversturm.com>
2017-03-09 16:46:47 +0000

    field config for pivot grid

--- webapp/static/pivotGrid.js
+++ webapp/static/pivotGrid.js
@@ -10,16 +10,67 @@ $(function() {
 		options: {
 		    text: "Reload grid",
 		    onClick: function() {
-			grid.refresh();
+			grid.getDataSource().reload();
 		    }
 		}
 	    }]
     });
 
     grid = $("#pivotGrid").dxPivotGrid({
+	fieldPanel: {
+	    visible: true
+	},
 	dataSource: {
 	    remoteOperations: true,
-	    store: dataStore
+	    store: dataStore,
+	    retrieveFields: false,
+	    fields: [
+		{
+		    dataField: "date1",
+		    dataType: "date",
+		    format: "shortDate",
+		    allowFiltering: false,
+		    allowSorting: true,
+		    allowSortingBySummary: true,
+		    area: "column"/*,
+		    groupInterval: "dayOfWeek"*/
+		},
+		{
+		    dataField: "date2",
+		    dataType: "date",
+		    format: "shortDate",
+		    allowFiltering: true,
+		    allowSorting: true,
+		    allowSortingBySummary: true,
+		    area: "filter",
+		    groupInterval: "quarter"
+		},
+		{
+		    dataField: "int1",
+		    dataType: "number",
+		    allowFiltering: true,
+		    allowSorting: true,
+		    allowSortingBySummary: true,
+		    area: "data"
+		},
+		{
+		    dataField: "int2",
+		    dataType: "number",
+		    allowFiltering: true,
+		    allowSorting: true,
+		    allowSortingBySummary: true,
+		    area: "filter",
+		    groupInterval: 10
+		},
+		{
+		    dataField: "string",
+		    dataType: "string",
+		    allowFiltering: true,
+		    allowSorting: true,
+		    allowSortingBySummary: true,
+		    area: "row"
+		}
+	    ]
 	}
     }).dxPivotGrid("instance");
 });
