cbc73659b3dfc2cbb42cf8e844dfc6d479ff885e
Oliver Sturm <oliver@oliversturm.com>
2017-06-05 16:35:55 +0100

    even less output

--- prepare-cloud9.sh
+++ prepare-cloud9.sh
@@ -22,7 +22,7 @@ sudo chmod +x /etc/init.d/mongod
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get -q update
+sudo apt-get -qq update
 sudo apt-get -q install -y mongodb-org
     
 /etc/init.d/mongod start
