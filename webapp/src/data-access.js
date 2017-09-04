86c0e4cb56380bdcf318774fdf342bdfae7e1752
Oliver Sturm <oliver@oliversturm.com>
2017-09-04 11:31:00 +0100

    updated to dx version alpha 8

--- webapp/src/data-access.js
+++ webapp/src/data-access.js
@@ -201,14 +201,9 @@ const createGroupQueryData = (data, loadOptions) => {
       return {
         _headerKey: `groupRow_${loadOptions.grouping[groupLevel].columnName}`,
         key: (parentGroupRow ? `${parentGroupRow.key}|` : '') + `${group.key}`,
-        colspan: parentGroupRow ? parentGroupRow.colspan + 1 : 1,
+        groupedBy: loadOptions.grouping[groupLevel].columnName,
         value: group.key,
-        type: 'groupRow',
-        column: {
-          name: loadOptions.grouping[groupLevel].columnName,
-          title: loadOptions.grouping[groupLevel].columnName
-        },
-        rows: []
+        type: 'groupRow'
       };
     }
 
