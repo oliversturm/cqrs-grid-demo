67017d65ea4c8c8f6c887414ec2b6eccd760eb29
Oliver Sturm <oliver@oliversturm.com>
2017-09-20 12:51:24 +0100

    fixed startup process, adding wait logic to services, disabling old debugger

--- query-service/index.js
+++ query-service/index.js
@@ -1,7 +1,22 @@
+const waitOn = require('wait-on');
+require('babel-polyfill');
 const seneca = require('seneca')();
 
-seneca.use('query-values').listen({
-  type: 'tcp',
-  port: process.env.QRYSRVC_PORT || 3001,
-  pin: 'role:entitiesQuery'
-});
+waitOn(
+  {
+    resources: [
+      `tcp:${process.env.MONGO_HOST || 'mongo'}:${process.env.MONGO_PORT || 27017}`
+    ]
+  },
+  waitErr => {
+    if (waitErr) {
+      console.error('Error waiting for resources: ', waitErr);
+      return;
+    }
+    seneca.use('query-values').listen({
+      type: 'tcp',
+      port: process.env.QRYSRVC_PORT || 3001,
+      pin: 'role:entitiesQuery'
+    });
+  }
+);
--- validator/start-service.sh
+++ validator/start-service.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 terminate() {
-	kill -TERM "$child" 2>/dev/null
+    kill -TERM "$child" 2>/dev/null
 }
 
-trap terminate TERM
-
-IP=`ip address | grep global | sed -r 's/.*inet (.*)\/.*/\1/'`
-
-echo "IP Address: $IP"
-
-node-inspector --no-preload --web-host=$IP --web-port=57575 &
-
-sleep 1
-
-node_modules/.bin/nodemon -I -V -- --harmony --debug index.js &
-child=$!
-wait "$child"
+if [ $DEPLOY ]; then
+    node_modules/.bin/forever -a -v -m 20 --minUptime 20000 --spinSleepTime 20000 --no-colors -c "node --harmony" index.js
+else
+    trap terminate TERM
+    IP=`ip address | grep global | sed -r 's/.*inet (.*)\/.*/\1/'`
+    node_modules/.bin/nodemon -I -V -- --harmony --inspect=$IP:9229 index.js &
+    child=$!
+    wait "$child"
+fi
