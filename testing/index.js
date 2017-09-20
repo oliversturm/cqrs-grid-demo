bea5e1a662199e90bfdbd16219c12acef9a6f214
Oliver Sturm <oliver@oliversturm.com>
2017-09-20 12:28:20 +0100

    fixed startup process, adding wait logic to services, disabling old debugger

--- testing/index.js
+++ testing/index.js
@@ -1,23 +1,36 @@
+const waitOn = require('wait-on');
 const seneca = require('seneca')(
   {
     //log: "all"
   }
 );
 
-console.log('Using connection: ', process.env.CMDSRVC_HOST);
+waitOn(
+  {
+    resources: [
+      `tcp:${process.env.CMDSRVC_HOST || 'command-service'}:${process.env.CMDSRVC_PORT || 3002}`
+    ]
+  },
+  waitErr => {
+    if (waitErr) {
+      console.error('Error waiting for resources: ', waitErr);
+      return;
+    }
 
-seneca
-  .client({
-    type: 'tcp',
-    // expecting "command-service" to be available
-    // as a docker link
-    host: process.env.CMDSRVC_HOST || 'command-service',
-    port: process.env.CMDSRVC_PORT || 3002,
-    pin: 'role:entitiesCommand'
-  })
-  .use('testing')
-  .listen({
-    type: 'tcp',
-    port: process.env.TESTSRVC_PORT || 3005,
-    pin: 'role:testing'
-  });
+    seneca
+      .client({
+        type: 'tcp',
+        // expecting "command-service" to be available
+        // as a docker link
+        host: process.env.CMDSRVC_HOST || 'command-service',
+        port: process.env.CMDSRVC_PORT || 3002,
+        pin: 'role:entitiesCommand'
+      })
+      .use('testing')
+      .listen({
+        type: 'tcp',
+        port: process.env.TESTSRVC_PORT || 3005,
+        pin: 'role:testing'
+      });
+  }
+);
--- web-proxy/index.js
+++ web-proxy/index.js
@@ -1,3 +1,4 @@
+const waitOn = require('wait-on');
 var seneca = require('seneca')();
 var web = require('seneca-web');
 var express = require('express');
@@ -15,65 +16,82 @@ function revive(key, value) {
   return fixValue(value, [fixDate]);
 }
 
-expressApp.use(
-  bodyParser.json({
-    reviver: revive
-  })
-);
-expressApp.use(cors());
+waitOn(
+  {
+    resources: [
+      `tcp:${process.env.QRYSRVC_HOST || 'query-service'}:${process.env.QRYSRVC_PORT || 3001}`,
+      `tcp:${process.env.CMDSRVC_HOST || 'command-service'}:${process.env.CMDSRVC_PORT || 3002}`,
+      `tcp:${process.env.VALSRVC_HOST || 'validator'}:${process.env.VALSRVC_PORT || 3003}`,
+      `tcp:${process.env.TESTSRVC_HOST || 'testing'}:${process.env.TESTSRVC_PORT || 3005}`
+    ]
+  },
+  waitErr => {
+    if (waitErr) {
+      console.error('Error waiting for resources: ', waitErr);
+      return;
+    }
 
-expressApp.use(require('morgan')('dev'));
+    expressApp.use(
+      bodyParser.json({
+        reviver: revive
+      })
+    );
+    expressApp.use(cors());
 
-var config = {
-  routes: routes,
-  adapter: require('seneca-web-adapter-express'),
-  context: expressApp,
-  options: {
-    parseBody: false
+    expressApp.use(require('morgan')('dev'));
+
+    var config = {
+      routes: routes,
+      adapter: require('seneca-web-adapter-express'),
+      context: expressApp,
+      options: {
+        parseBody: false
+      }
+    };
+
+    seneca
+      .use('proxy')
+      .client({
+        type: 'tcp',
+        // expecting "query-service" to be available
+        // as a docker link
+        host: process.env.QRYSRVC_HOST || 'query-service',
+        port: process.env.QRYSRVC_PORT || 3001,
+        pin: 'role:entitiesQuery'
+      })
+      .client({
+        type: 'tcp',
+        // expecting "command-service" to be available
+        // as a docker link
+        host: process.env.CMDSRVC_HOST || 'command-service',
+        port: process.env.CMDSRVC_PORT || 3002,
+        pin: 'role:entitiesCommand'
+      })
+      .client({
+        type: 'tcp',
+        // expecting "validator" to be available
+        // as a docker link
+        host: process.env.VALSRVC_HOST || 'validator',
+        port: process.env.VALSRVC_PORT || 3003,
+        pin: 'role:validation'
+      })
+      .client({
+        type: 'tcp',
+        // expecting "testing" to be available
+        // as a docker link
+        host: process.env.TESTSRVC_HOST || 'testing',
+        port: process.env.TESTSRVC_PORT || 3005,
+        pin: 'role:testing'
+      })
+      .use(web, config)
+      .ready(() => {
+        var server = seneca.export('web/context')();
+
+        var port = process.env.WEBPROXY_PORT || 3000;
+
+        server.listen(port, () => {
+          console.log('Web Proxy running on port ' + port);
+        });
+      });
   }
-};
-
-seneca
-  .use('proxy')
-  .client({
-    type: 'tcp',
-    // expecting "query-service" to be available
-    // as a docker link
-    host: process.env.QRYSRVC_HOST || 'query-service',
-    port: process.env.QRYSRVC_PORT || 3001,
-    pin: 'role:entitiesQuery'
-  })
-  .client({
-    type: 'tcp',
-    // expecting "command-service" to be available
-    // as a docker link
-    host: process.env.CMDSRVC_HOST || 'command-service',
-    port: process.env.CMDSRVC_PORT || 3002,
-    pin: 'role:entitiesCommand'
-  })
-  .client({
-    type: 'tcp',
-    // expecting "validator" to be available
-    // as a docker link
-    host: process.env.VALSRVC_HOST || 'validator',
-    port: process.env.VALSRVC_PORT || 3003,
-    pin: 'role:validation'
-  })
-  .client({
-    type: 'tcp',
-    // expecting "testing" to be available
-    // as a docker link
-    host: process.env.TESTSRVC_HOST || 'testing',
-    port: process.env.TESTSRVC_PORT || 3005,
-    pin: 'role:testing'
-  })
-  .use(web, config)
-  .ready(() => {
-    var server = seneca.export('web/context')();
-
-    var port = process.env.WEBPROXY_PORT || 3000;
-
-    server.listen(port, () => {
-      console.log('Web Proxy running on port ' + port);
-    });
-  });
+);
