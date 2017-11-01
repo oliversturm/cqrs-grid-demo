6e48f0704309481e56c33cefedf8fc31e0d9dce8
Oliver Sturm <oliver@oliversturm.com>
2017-11-01 14:38:44 +0000

    removing filter param workarounds

--- webapp/src/data-access.js
+++ webapp/src/data-access.js
@@ -26,20 +26,8 @@ const getFilterParams = loadOptions =>
   loadOptions.filters && loadOptions.filters.length > 0
     ? {
         filter: loadOptions.filters.reduce((r, v) => {
-          // This 'if' is a workaround - items in the filters collection
-          // currently stay in the list when the user enters a value
-          // in a filter editor and then deletes it again, only the value
-          // field is then empty. So I'm currently ignoring filtering
-          // items with empty 'value's -- not quite the right way, since
-          // I believe empty values could be valid filters.
-          if (v.value) {
-            if (r.length > 0) r.push('and');
-            r.push([
-              v.columnName,
-              '=',
-              v.columnName === 'int1' ? parseInt(v.value, 10) : v.value
-            ]);
-          }
+          if (r.length > 0) r.push('and');
+          r.push([v.columnName, '=', v.value]);
           return r;
         }, [])
       }
